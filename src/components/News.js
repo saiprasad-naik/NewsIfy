ffbc94c1e5e74bb953d534f3a9ede5e743d847fd
Saiprasad Naik <saiprasadnaikpersonal11@gmail.com>
2023-06-26 14:42:14 +0530

    new

---
+++ src/components/News.js
@@ -0,0 +1,18 @@
+import userEvent from '@testing-library/user-event';
+import React from 'react'
+import { useEffect } from 'react';
+
+export default function News() {
+    const url = "http://hn.algolia.com/api/v1/search?query=html";
+
+    useEffect(()=>{
+        
+    })
+
+  return (
+
+    <div>
+      
+    </div>
+  )
+}
